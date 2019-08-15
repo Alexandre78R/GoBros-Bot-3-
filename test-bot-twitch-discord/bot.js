7541acde6beba86fdac9867dea54651210e14d2c
Alexandre78R <alexandre.renard98@gmail.com>
2019-08-15 22:35:37 +0200

    Update structure

--- test-bot-twitch-discord/bot.js
+++ test-bot-twitch-discord/bot.js
@@ -16,17 +16,24 @@ const app = express();
 //Génération du port random
 app.set('port', (process.env.PORT || Math.floor(Math.random() * Math.floor(5000))))
 
-// app.listen(app.get('port'), function(){
-//     console.log(`Le bot fonctionne sur le port : ${app.get('port')} `);
-// })
+app.listen(app.get('port'), function(){
+    console.log(`[Discord] : Le bot fonctionne sur le port : ${app.get('port')} `);
+})
 
-//console allumage du bot sur discord
+//En cas d'erreur pour le bot discord
+bot.on('warn', console.warn);
+bot.on('error', console.error);
+bot.on('disconnect', () => console.log('Je viens de me déconnecter, en m\'assurant que vous savez, je vais me reconnecter maintenant'));
+bot.on('reconnecting', () => console.log('Je reconnecte maintenant !'));
+
+//Clé du bot
+bot.login('clé discord');
+
+//Status du bot discord 
 bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.tag}!`);
+	bot.user.setPresence({ game: { name: "En développement By Alexandre78R", type : "STREAMING", url: "https://www.twitch.tv/jaxoou"}});
+	console.log(`[Discord] : Connecté en tant que ${bot.user.tag}`)
 });
-
-//clé du bot
-bot.login('clé du bot discord');
 
 //Option à la connexion de twitch
 const options = {
@@ -38,10 +45,10 @@ const options = {
 		reconnect: true,
 	},
 	identity: {
-		username : '< pseudo du bot >',
-		password: 'oauth:********************************',
+		username : 'username',
+		password: 'oauth:***************************',
 	},
-	channels : ['channels du bot'],
+	channels : ['alexandre78rg'],
 };
 
 //Mise en place du bot twitch avec les options sur client
@@ -52,7 +59,8 @@ client.connect();
 
 //Message d'allumage du bot sur le tchat twitch
 client.on('connected', (adress, port) => {
-	client.action('channels du bot', 'Le bot est en ligne');
+    console.log("[Twitch] : " + client.getUsername() + " s'est connecté sur : " + adress + ", port : " + port);
+    client.action('alexandre78rg', 'Bonjour , je suis le bot en configuration. j"arrive avec le compte bot !');
 });
 
 //Commande test pour discord
@@ -70,11 +78,58 @@ bot.on('message', message => {
 	}
   });
 
-  //Gestion des commandes sur twitch
+  //Prefix de commande pour twitch 
+  const prefix = "!";
+
+  //Gestion prefix
+  function commandParser(message){
+    let prefixEscaped = prefix.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+    let regex = new RegExp("^" + prefixEscaped + "([a-zA-Z]+)\s?(.*)");
+    return regex.exec(message);
+  }
+
+  //Gestion log discord
+  function log(){
+	// bot.on('message', msg => {
+	// 	let reportEmbed = new Discord.RichEmbed()
+	// 	.setTitle("Test message")
+	// 	.setColor("#15f153")
+	// 	.addField("Test message", `test`)
+	// 	let reportschannel = msg.guild.channels.find(`name`, "test-dev");
+	// 	if(!reportschannel) return msg.channel.send(console.log("error channel"));
+
+	// 	reportschannel.send(reportEmbed);
+	// });
+	console.log("Test Log")
+  }
+ 
+//Gestion des commandes sur twitch
 client.on('chat', (channel, user, message, self) => {
-    //Commande test pour twitch
-	if (message === '!test'){
-		client.action('channels du bot', `${user['display-name']}, Vous avez taper la commande !test !`)
-		console.log("test")
-	}
+	//Si le message via du bot il stop les fonctions.
+	if (self) return;
+
+	// client.say(channel, user['display-name'] + " a dit " + message );
+	let commands = commandParser(message);
+
+	//Les agument des commandes après le prefix
+	if(commands){
+        let command = commands[1];
+        // let param = commands[2];
+		
+		//Liste des commandes
+        switch(command){
+
+			//Commande de test
+            case "test":
+                client.say(channel, `${user['display-name']}, Vous avez taper la commande !test !`)
+				log();
+				// bot.on('ready', () => { console.log("test")});
+			break;
+			
+			//Message d'erreur si la commande n'existe pas.
+            default:
+               client.say(channel, `${user['display-name']}, La Commande '` + command + "'' est non reconnue. Tapez " + prefix + "help pour la liste des commandes de " + client.getUsername());
+        }
+    }
+	
 });
